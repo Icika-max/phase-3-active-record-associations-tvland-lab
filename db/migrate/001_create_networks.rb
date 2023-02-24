936570b142da8e936091c329814ebe13fcb60e5d
icika-max <icikakennedy@gmail.com>
2023-02-24 14:07:00 +0300

    completed assignment

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -9,10 +9,10 @@ class CreateNetworks < ActiveRecord::Migration[6.1]
     create_table :networks do |t| # we get a block variable here for the table
       # primary key of :id is created for us!
       # defining columns is as simple as t.[datatype] :column
-      t.string :call_letters
+      t.string :channel
       # the above breaks down to
       # "create a column called :call_letters on table t with type string
-      t.integer :channel
+      t.integer :call_letters
     end
   end
 end
